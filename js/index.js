170a64ac442b03c5a574d30868b0bc6dd5ffe9e0
wemadeit2022 <compass@circles.asia>
2023-01-03 19:09:02 +0630

    Add console.log conflict state by dev2

--- js/index.js
+++ js/index.js
@@ -29,3 +29,5 @@ console.log("Add console by nyihtutlwin");
 console.log("Add console by dev1");
 console.log("Add console by dev2");
 console.log("Add console by dev1 on github");
+
+console.log("Add console.log by conflict state by dev2");
